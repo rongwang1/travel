dfa6bcb49ff40a8e41b67650a583ef2241c27699
rongwang1 <614538673@qq.com>
2019-08-13 15:02:51 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//npm install fastclick --save 安装fastclick 是为了解决移动端点击300毫秒延迟的问题
+import fastClick from 'fastclick'
+//样式重置css文件
+import './assets/styles/reset.css'
+//由于不同的手机有不同的像素密度，css中的1px不等于设备中的1px,所以在项目中我们引入border.css解决这个问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +20,4 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+//路由就是根据网址的不同，返回不同的内容给客户
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  hello word
+</div>
+</template>
+<script>
+export default {
+    name:'home'
+}
+</script>
+<style>
+</style>
