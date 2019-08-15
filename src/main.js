3a87ddfcbccdd3d3982f59bff6b52d5287df7ed0
rongwang1 <614538673@qq.com>
2019-08-15 16:27:09 +0800

    change

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 //npm install fastclick --save 安装fastclick 是为了解决移动端点击300毫秒延迟的问题
 import fastClick from 'fastclick'
 //样式重置css文件
@@ -10,9 +11,11 @@ import 'styles/reset.css'
 //由于不同的手机有不同的像素密度，css中的1px不等于设备中的1px,所以在项目中我们引入border.css解决这个问题
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="wrapper">
+ <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList"  :key="item.id"><img  class="swiper-img" :src="item.imgUrl" alt="#"></swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+<script>
+export default{
+     name:'HomeSwiper',
+     data (){
+         return{
+             swiperOption:{
+                 pagination:'.swiper-pagination',
+                 loop:true
+             },
+             swiperList:[
+                 {id:'01',imgUrl:'//m.360buyimg.com/mobilecms/s750x366_jfs/t1/23112/33/6431/99212/5c540d7fEf8000223/fddb6b047c02ba3d.jpg!cr_1125x549_0_72!q70.jpg.dpg'},
+                 {id:'02',imgUrl:'//m.360buyimg.com/mobilecms/s750x366_jfs/t1/45822/37/1258/360866/5cefb71fE95298b80/052ef4b9d125778a.jpg!cr_1125x549_0_72!q70.jpg.dpg'}
+             ]
+         }
+     }
+     
+}
+</script>
+<style  lang="stylus" scoped>
+//      >>>的意思是穿透：引用了第三方组件，需要在组件中局部修改第三方组件的样式
+//      而又不想去除scoped属性造成组件之间的样式污染。此时只能通过特殊的方式，穿透scoped
+.wrapper>>>.swiper-pagination-bullet
+   background-color #fff
+.wrapper
+    // 设置该处的样式的作用是防止网速过慢导致加载页面抖动
+   overflow hidden
+   width 100%
+   height 0
+   padding-bottom  48.8%
+  .swiper-img
+      width 100%
+
+</style>
