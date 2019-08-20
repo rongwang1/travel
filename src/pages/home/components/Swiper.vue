9f2724aa3daf497edccf9b09dde4854dbaaa2c8d
rongwang1 <614538673@qq.com>
2019-08-20 09:40:58 +0800

    fix bug

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -15,7 +15,7 @@ export default{
      },
      data (){
          return{
-             swiperOption:{
+             swiperOption: {
                  pagination:'.swiper-pagination',
                  loop:true
              }
