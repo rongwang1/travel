8047ec77580f52e02911097168c1690891c3b961
rongwang1 <614538673@qq.com>
2019-08-27 12:59:03 +0800

    detailHeader

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+<div>
+    <router-link class='header-abs' tag='div'  to='/'  v-show="showAbs">
+            <div class='iconfont  header-abs-back'>&#xe710;</div>
+    </router-link>
+    <div class='header-fixed' v-show="!showAbs" :style="opacityStyle">
+     <router-link tag='div'  to='/'>
+            <div class='iconfont  header-fixed-back'>&#xe710;</div>
+            景点详情
+    </router-link>
+    </div>
+</div>
+</template>
+<script>
+export default {
+ name:'DetailHeader',
+ data () {
+     return {
+         showAbs:true,
+         opacityStyle: {
+            opacity:0
+         }
+     }
+ },
+ methods:{
+     handleScroll(){
+       const top =document.documentElement.scrollTop
+       if(top>60){
+           let opacity=top/140
+           opacity=opacity>1? 1:opacity
+           this.opacityStyle={opacity}
+           this.showAbs=false
+       }  else{
+           this.showAbs=true
+       }
+     }
+ },
+ mounted (){
+     window.addEventListener('scroll',this.handleScroll)
+ },
+ unmounted(){
+     window.removeEventListener('scroll',this.handleScroll)
+ }
+}
+</script>
+<style lang="stylus"  scoped>
+@import '~styles/varibles.styl'
+.header-abs
+   position absolute
+   left .2rem
+   top .2rem
+   width .8rem
+   height .8rem
+   line-height  .8rem
+   text-align center
+   border-radius .4rem
+   background:rgba(0,0,0,.4rem)
+   .header-abs-back
+      color #ffffff
+      font-size .6rem
+.header-fixed
+   position fixed
+   top 0 
+   left 0 
+   right 0 
+   height $headerHeight
+   line-height  $headerHeight
+   background-color $bgColor
+   font-size .32rem
+   color #ffffff
+   text-align center
+   .header-fixed-back
+      position absolute
+      top 0 
+      left 0 
+      width .64rem
+      color #ffffff
+      font-size .6rem   
+
+</style>
