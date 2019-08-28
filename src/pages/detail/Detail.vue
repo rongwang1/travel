ce6eaa54915de29284d92fe3cfddafda138ff854
rongwang1 <614538673@qq.com>
2019-08-28 23:09:57 +0800

    detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,17 +2,46 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
+        <detail-list :list="list"></detail-list>
         <div class='content'></div>
     </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
    name:"Detail",
    components: {
        DetailBanner,
-       DetailHeader
+       DetailHeader,
+       DetailList
+   },
+   data(){
+       return {
+           list:[
+                {
+                title: "学生票",
+                children:[
+                    {
+                     title:"学生票系列一",
+                     children:[{
+                         title:"博物馆",
+                     },{
+                         title:"科技馆"
+                     }]
+                    },
+                    {
+                     title:"学生票系列二"
+                    },
+                ]
+               },{
+                title:"儿童票"
+               },{
+                title:"成人票"
+               }
+           ]
+       }
    }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+ <div v-for="(item,index) of list"   :key="index">
+        <div class="item-title  border-bottom">
+            <span class="item-title-icon"></span>
+            {{item.title}}
+        </div> 
+          <!-- 递归数组 -->
+        <div v-if="item.children"  class="item-children">
+            <detail-list :list="item.children"></detail-list>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name:"DetailList",
+    props:{
+        list:Array
+    }
+}
+</script>
+<style lang="stylus"  scoped>
+.item-title
+  line-height .8rem
+  font-size .32rem
+  padding 0  .2rem
+  .item-title-icon
+    position relative
+    display inline-block
+    width: .36rem
+    height: .36rem
+    top: .09rem
+    left: .06rem
+    background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+    margin-right: .1rem
+    background-size: .4rem 3rem
+.item-children
+    padding 0 .3rem
+</style>
